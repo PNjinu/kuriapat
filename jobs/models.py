597fe237946a24d938de217919099fb5dcf658e0
Patrick <kuria144@gmail.com>
2018-11-26 00:13:27 +0300

    Changed the model to include link and progess status

--- jobs/models.py
+++ jobs/models.py
@@ -6,6 +6,18 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    link = models.CharField(max_length=500)
+    progress = "In Progress"
+    done = "Done"
+    status_choices = (
+        (progress, 'In progress'),
+        (done, 'Done')
+    )
+    status = models.CharField(
+        max_length=11,
+        choices=status_choices,
+        default=done,
+    )
 
     def __str__(self):
         return self.summary
